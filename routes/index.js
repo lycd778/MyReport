6487ffe2f8d58c3b7ea6ab268af97d4e05589c88
lingxiao-Qin_work <lingxiao.qin@easyhealth365.com>
2016-11-02 11:59:44 +0800

    我的报告主页

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '我的报告' });
-});
 
+
+router.get('/', function (req, res, next) {
+    var personStr = '[\
+        {\
+            "checkdatime" : "2016-10-26",\
+            "typename" :"心肺评估",\
+            "typecode" : "1"\
+        },\
+        {\
+            "checkdatime" : "2016-11-01",\
+            "typename" : "心脏彩超",\
+            "typecode" : "1"\
+        },\
+        {\
+            "checkdatime" : "2016-11-02",\
+            "typename" : "尿常规",\
+            "typecode" : "0"\
+        },\
+       {\
+            "checkdatime" : "2016-11-03",\
+            "typename" : "血脂",\
+            "typecode" : "0"\
+        },\
+        {\
+            "checkdatime" : "2016-09-11",\
+            "typename" : "血常规",\
+            "typecode" : "0"\
+        }\
+    ]';
+    var persons = JSON.parse(personStr);
+    res.render('index', {title: '我的报告', person: persons});
+});
 module.exports = router;
