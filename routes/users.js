f135ea1fa72f46e8d2dceeb093113464f52eca49
lingxiao-Qin_work <lingxiao.qin@easyhealth365.com>
2016-11-09 15:54:29 +0800

    实验室内容页面json

--- routes/users.js
+++ routes/users.js
@@ -8,37 +8,16 @@ router.get('/', function (req, res1, next) {
     var recordid=req.query.ReportId;
     var type=req.query.type;
     var userid=req.query.userid;
-    var url='http://123.57.143.76:8010/api/qq/api/qq/Reportsdetial?recordid='+recordid+'&&type='+type+'&&userid='+userid;
-    //var url='http://123.57.143.76:8010/api/qq/Reportsdetial?recordid=1cc97ed8-48ad-4e16-89d5-044523d72135&&type=0&&userid=f7076924-a2ae-4727-a098-331b64ef877e';
+    var url='http://123.57.143.76:8010/api/qq/Reportsdetial?recordid='+recordid+'&&type='+type+'&&userid='+userid;
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            detial = null;
             var date=JSON.parse(body);
+            detial = null;
             detial = date.results;
-            console.log(detial);
+            console.log("detial: \n"+body);
             res1.render('lab',{title: '我的列表', detial: detial});
         }
     });
 
 });
-router.get('/lab', function (req, res1, next) {
-    var recordid=req.query.ReportId;
-    var type=req.query.type;
-    var userid=req.query.userid;
-    var url='http://123.57.143.76:8010/api/qq/api/qq/Reportsdetial?recordid='+recordid+'&&type='+type+'&&userid='+userid;
-    //var url='http://123.57.143.76:8010/api/qq/Reportsdetial?recordid=1cc97ed8-48ad-4e16-89d5-044523d72135&&type=0&&userid=f7076924-a2ae-4727-a098-331b64ef877e';
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            detial = null;
-            var date=JSON.parse(body);
-            detial = date.results;
-            console.log(detial);
-            res1.render('lab',{title: '我的列表', detial: detial});
-        }
-    });
-
-});
-//router.get('/', function (req, res, next) {
-//    res.render(date);
-//});
 module.exports = router;
